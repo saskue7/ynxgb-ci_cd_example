34b4a56cc72fc3b1383c3059af0a7cb769476fd8
saskue7 <55278429+saskue7@users.noreply.github.com>
2023-03-09 16:30:10 +0530

    Update server.js

--- sample/server.js
+++ sample/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end("Hello World!");
+   response.end("Hello World! you thought tht was it hahhahha amf you have not seen the world u lazy bum");
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
